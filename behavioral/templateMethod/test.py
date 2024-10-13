71e77539f6d27d02df0a14ab90aae9a2658b4eea
Akshansh Gusain <akshanshgusain@gmail.com>
2024-10-13 16:52:39 +0530

    feat(behavioral): add template pattern test

---
+++ behavioral/templateMethod/test.py
@@ -0,0 +1,8 @@
+from behavioral.templateMethod.templateMethod import Tea, Coffee
+
+if __name__ == "__main__":
+    my_tea = Tea()
+    my_tea.prepare_recipe()
+
+    my_coffee = Coffee()
+    my_coffee.prepare_recipe()
